ac4ffff50396d6a7d01fed5105348e85d3237259
Izutech <inspire.ig57@gmail.com>
2023-06-11 03:36:30 +0100

    Add files via upload

--- reserse.js
+++ reserse.js
@@ -1,12 +1,17 @@
 let input = document.getElementById('input');
 let button = document.getElementById('reverse');
 let result = document.getElementById('result');
+let initial = document.getElementById('Original');
+let arrow = document.getElementById('p');
 function reverse(){
+    initial.innerHTML = input.value;
+    arrow.innerHTML = '===>'
     let inputValue = input.value;
     let list = [];
     for(let i = inputValue.length - 1; i >= 0; i -=1){
         list.push(inputValue[i])
     }
+    
     result.innerHTML = list.join('');
     input.value = '';
 }
